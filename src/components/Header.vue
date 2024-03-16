d1164ef675ad4f1758c618452b8306652763f90b
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-03-16 18:49:24 -0300

    feature: create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="header">
+           <img src="../assets/logo.svg" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
