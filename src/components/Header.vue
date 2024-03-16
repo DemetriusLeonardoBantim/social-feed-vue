631d241e7c70529d410e0221bdba96730a0871e5
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-03-16 19:23:25 -0300

    feature: create style component header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="header">
-           <img src="../assets/logo.svg" />
+           <img src="../assets/logo.svg"  class="header__icon" />
+           <h1 class="header__title">Ignite Feed</h1>
     </div>
 </template>
 
@@ -11,3 +12,23 @@ defineProps<{ msg: string }>()
 
 const count = ref(0)
 </script>
+
+<style scoped lang="scss">
+    .header {
+        background-color: #202024;
+        display: flex;
+        padding: 20px;
+        align-items: center;
+        justify-content: center;
+
+        &__icon {
+            margin-right: 1rem;
+        }
+    
+        &__title {
+            color: #fff;
+            font-weight: bold;
+            letter-spacing: 0;
+        }
+    }
+</style>
