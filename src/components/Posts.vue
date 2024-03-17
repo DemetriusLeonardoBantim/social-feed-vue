76cd8419845fe6644d442fb418431f4c6348cd3b
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-03-17 06:49:08 -0300

    feature: create component posts

---
+++ src/components/Posts.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<style scoped lang="scss">
+    .container {
+        max-height: 621px;
+        max-width: 830px;
+        border-radius: 8px;
+        background: #323238;
+        padding: 40px;
+    }
+</style>
